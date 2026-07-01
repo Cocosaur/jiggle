1b817a8a481e136633736a22b876cc7920bf6a0e
Cocosaur <Cocosaur@users.noreply.github.com>
2026-07-01 12:47:34 +0200

    goal and scoring system

--- Assets/Scripts/Sc_goal.cs
+++ Assets/Scripts/Sc_goal.cs
@@ -1,16 +1,80 @@
+using System;
 using UnityEngine;
+using TMPro;
 
 public class Sc_goal : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int countPurple;
+    private int countPink;
+    private int countUnicorn;
+    public bool winCondition;
+        
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI countTextPrpl;
+    public TextMeshProUGUI countTextPnk;
+    
+    private void OnCollisionEnter(Collision other)
     {
+        if (other.collider.CompareTag("player-purple"))
+        {
+            countPink = countPink + 1;
+            Debug.Log("player pink has scored" + countPink);
+            
+            SetCountText();
+
+        }
+        
+        if (other.collider.CompareTag("player-pink"))
+        { 
+            countPurple = countPurple + 1;
+           Debug.Log("player purple has scored" + countPurple);
+           
+           SetCountText();
+        }
+        
+        if (other.collider.CompareTag("unicorn"))
+        {
+            countUnicorn = countUnicorn + 1;
+            Debug.Log("player has scored" + countUnicorn);
+            
+            SetCountText();
+
+        }
         
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        SetCountText();
+        winCondition = false;
+
+    }
+    
+    void SetCountText()
+    {
+        //countText.text = "Dummy Score " + countUnicorn.ToString();
+        countTextPrpl.text = "Purple Score " + countPurple.ToString();
+        countTextPnk.text = "Pink Score " + countPink.ToString();
+    }
+
     void Update()
     {
+        if (countUnicorn.Equals(3))
+        { 
+            winCondition = true;
+            Debug.Log("Player has won");
+        }
         
+        if (countPurple.Equals(3))
+        { 
+            winCondition = true;
+            Debug.Log("Pink has won");
+        }
+        
+        if (countPink.Equals(3))
+        { 
+            winCondition = true;
+            Debug.Log("Purple has won");
+        }
     }
 }
